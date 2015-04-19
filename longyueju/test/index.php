2e508038026d3df9b4cd3c6d9ed88186cefcc509
zh597588308 <zh597588308@sina.com>
2015-04-19 23:33:57 +0800

    learn imooc, do longyueju netpage

---
+++ longyueju/test/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>欢迎学习php!</title>
+    </head>
+	<body>
+       <p>
+       <?php 
+       echo 'PHP学到家，走到哪儿都不怕！';
+       
+       
+       
+       ?></p>
+       
+	</body>
+</html>

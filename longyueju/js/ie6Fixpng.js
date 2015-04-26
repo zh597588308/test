14c99f9e2de98670a3dfc26990827f92f4fd6626
zh597588308 <zh597588308@sina.com>
2015-04-26 23:56:42 +0800

    增加产品分类页面，主页面支持iphone的safira浏览器，小图片多2px
    Signed-off-by: zh597588308 <zh597588308@sina.com>

---
+++ longyueju/js/ie6Fixpng.js
@@ -0,0 +1,37 @@
+//JavaScript Document
+DD_belatedPNG.fix("div,ul,ol,li,dt,dd,dl,span,img,a,em,strong,h1,h2,h3,h4,h5,h6,p");
+//DD_belatedPNG.fix("button");
+//DD_belatedPNG.fix("a");
+//DD_belatedPNG.fix("h1");
+//DD_belatedPNG.fix("h5");
+//DD_belatedPNG.fix("form");
+//DD_belatedPNG.fix("p img");
+//DD_belatedPNG.fix("a img");
+//DD_belatedPNG.fix("img");
+//DD_belatedPNG.fix("input");
+//DD_belatedPNG.fix("li");
+//DD_belatedPNG.fix("td");
+//DD_belatedPNG.fix("span");
+//DD_belatedPNG.fix("table tr td");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
